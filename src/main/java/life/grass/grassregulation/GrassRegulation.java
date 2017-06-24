9031bda479b564e6c2f7a33e8024b09924370416
cyclic-ester <Cyclic_Ester@yahoo.co.jp>
2017-06-24 11:55:18 +0900

    [add] prevent items

--- src/main/java/life/grass/grassregulation/GrassRegulation.java
+++ src/main/java/life/grass/grassregulation/GrassRegulation.java
@@ -1,17 +1,76 @@
 package life.grass.grassregulation;
 
 import life.grass.grassregulation.event.RegulationEvent;
+import org.bukkit.Material;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Iterator;
 
 public final class GrassRegulation extends JavaPlugin {
 
+    private static GrassRegulation instance;
+
+    private static GrassRegulation getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+
+        instance = this;
         getServer().getPluginManager().registerEvents(new RegulationEvent(), this);
+        removeRecipes();
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private static void removeRecipes() {
+
+        Iterator<Recipe> iterator = getInstance().getServer().recipeIterator();
+        while (iterator.hasNext()) {
+            Recipe r = iterator.next();
+            if (isCraftProhibitedItem(r.getResult().getType())) {
+                iterator.remove();
+            }
+        }
+
+    }
+
+    private static boolean isCraftProhibitedItem(Material item) {
+
+        return item.equals(Material.ENCHANTMENT_TABLE) ||
+                item.equals(Material.ANVIL) ||
+                item.equals(Material.BREWING_STAND_ITEM) ||
+                item.equals(Material.HOPPER) ||
+                item.equals(Material.COAL_BLOCK) ||
+                item.equals(Material.IRON_BLOCK) ||
+                item.equals(Material.GOLD_BLOCK) ||
+                item.equals(Material.LAPIS_BLOCK) ||
+                item.equals(Material.EMERALD_BLOCK) ||
+                item.equals(Material.REDSTONE_BLOCK) ||
+                item.equals(Material.DIAMOND_BLOCK) ||
+                item.equals(Material.GOLD_NUGGET) ||
+                item.equals(Material.GOLD_INGOT) ||
+                item.equals(Material.IRON_INGOT) ||
+                item.equals(Material.IRON_NUGGET) ||
+                item.equals(Material.CAKE) ||
+                item.equals(Material.BREAD) ||
+                item.equals(Material.PUMPKIN_PIE) ||
+                item.equals(Material.MUSHROOM_SOUP) ||
+                item.equals(Material.BEETROOT_SOUP) ||
+                item.equals(Material.COOKED_BEEF)||
+                item.equals(Material.COOKED_CHICKEN) ||
+                item.equals(Material.COOKED_FISH) ||
+                item.equals(Material.GRILLED_PORK) ||
+                item.equals(Material.COOKED_MUTTON) ||
+                item.equals(Material.COOKED_RABBIT) ||
+                item.equals(Material.BAKED_POTATO) ||
+                item.equals(Material.RABBIT_STEW);
+
     }
 }
--- src/main/java/life/grass/grassregulation/event/RegulationEvent.java
+++ src/main/java/life/grass/grassregulation/event/RegulationEvent.java
@@ -1,27 +1,11 @@
 package life.grass.grassregulation.event;
 
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class RegulationEvent implements Listener {
-
-    @EventHandler
-    public void onCraftItem(PrepareItemCraftEvent event) {
-
-        Material item = event.getRecipe().getResult().getType();
-
-        if (isProhibitedItem(item)) {
-
-            event.getInventory().setResult(new ItemStack(Material.AIR));
-
-        }
-
-    }
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
@@ -32,34 +16,11 @@ public class RegulationEvent implements Listener {
 
     @EventHandler
     public void onPlayerTeleport(PlayerTeleportEvent event) {
-        event.getPlayer().sendMessage("-----^-^-^-^--^--^-^--^-^-");
 
         if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.NETHER_PORTAL)) {
 
             event.setCancelled(true);
 
         }
-
     }
-
-    private static boolean isProhibitedItem(Material item) {
-
-        return item.equals(Material.ENCHANTMENT_TABLE) ||
-                item.equals(Material.ANVIL) ||
-                item.equals(Material.BREWING_STAND_ITEM) ||
-                item.equals(Material.HOPPER) ||
-                item.equals(Material.COAL_BLOCK) ||
-                item.equals(Material.IRON_BLOCK) ||
-                item.equals(Material.GOLD_BLOCK) ||
-                item.equals(Material.LAPIS_BLOCK) ||
-                item.equals(Material.EMERALD_BLOCK) ||
-                item.equals(Material.REDSTONE_BLOCK) ||
-                item.equals(Material.DIAMOND_BLOCK) ||
-                item.equals(Material.GOLD_NUGGET) ||
-                item.equals(Material.GOLD_INGOT) ||
-                item.equals(Material.IRON_INGOT) ||
-                item.equals(Material.CAKE);
-
-    }
-
 }
